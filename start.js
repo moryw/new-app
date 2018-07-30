33a51d20050cf330fd12fe82e35666adca5ed415
moryw <matt@skitskat.xyz>
2018-07-30 13:54:13 +0700

    Setup and define mongoose schema and save to DB

--- start.js
+++ start.js
@@ -11,6 +11,8 @@ mongoose.connection
     console.log(`Connection error: ${err.message}`);
   });
 
+require('./models/Registration');
+
 const app = require('./app');
 
 const server = app.listen(3000, () => {
