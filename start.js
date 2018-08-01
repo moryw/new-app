4aaafaefef632dd8fdc869b339d22cf0fcf82d3d
moryw <matt@skitskat.xyz>
2018-08-01 09:08:28 +0700

    Edit procfile

--- start.js
+++ start.js
@@ -13,8 +13,8 @@ mongoose.connection
 
 require('./models/Registration');
 
-// const app = require('./app');
-//
-// const server = app.listen(3000, () => {
-//   console.log(`Express is running on port ${server.address().port}`);
-// });
+const app = require('./app');
+
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
